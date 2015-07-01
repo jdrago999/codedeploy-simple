34339896eee7825175db07aa42e985f38b6a8b7a
John Drago <jdrago.999@gmail.com>
2015-07-01 11:59:32 -0700

    Added comment

--- lib/codedeploy.rb
+++ lib/codedeploy.rb
@@ -4,6 +4,7 @@ require 'active_support/core_ext/hash/indifferent_access'
 require 'active_support/core_ext/string/inflections'
 
 module Codedeploy
+  # Module constants:
   AWS_DEFAULT_REGION = ENV['AWS_DEFAULT_REGION'] || 'us-east-1'
   DEBUG = ENV['DEBUG'] || false
   DEBUG_AWS = ENV['DEBUG_AWS'] || false
